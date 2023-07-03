3537d45d324261601a30ff06e88a30d7a0c8bc0f
Juangmcz <juangui-089@hotmail.com>
2023-07-03 15:41:09 -0500

    imported multiple new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,51 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HttpClientModule } from '@angular/common/http';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ManageUsersPageComponent } from './pages/manage-users-page/manage-users-page.component';
+import { CreateSubscriberPageComponent } from './pages/create-subscriber-page/create-subscriber-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     LoginPageComponent,
-    SidenavbarComponent
+    SidenavbarComponent,
+    NotFoundPageComponent,
+    ManageUsersPageComponent,
+    CreateSubscriberPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
