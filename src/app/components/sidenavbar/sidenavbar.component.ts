097e50bd74db0bd5d7b4695a53fd23a9017d764e
Juangmcz <juangui-089@hotmail.com>
2023-07-03 15:39:08 -0500

    implemented sidenavbar

--- src/app/components/sidenavbar/sidenavbar.component.ts
+++ src/app/components/sidenavbar/sidenavbar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenavbar',
   templateUrl: './sidenavbar.component.html',
-  styleUrls: ['./sidenavbar.component.scss']
+  styleUrls: ['./sidenavbar.component.scss'],
 })
 export class SidenavbarComponent {
+  constructor(private router: Router) {}
 
+  onLogout(): void {
+    console.log(localStorage.getItem('Token'));
+    this.router.navigate(['login']);
+    localStorage.removeItem('Token');
+  }
 }
