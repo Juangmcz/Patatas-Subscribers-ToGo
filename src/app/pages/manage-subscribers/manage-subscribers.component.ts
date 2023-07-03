69c0924aa580af8d45686ed124794a1e9dbb6ef4
Juangmcz <juangui-089@hotmail.com>
2023-07-03 17:00:59 -0500

    created manage subscribers page

---
+++ src/app/pages/manage-subscribers/manage-subscribers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-subscribers',
+  templateUrl: './manage-subscribers.component.html',
+  styleUrls: ['./manage-subscribers.component.scss']
+})
+export class ManageSubscribersComponent {
+
+}
