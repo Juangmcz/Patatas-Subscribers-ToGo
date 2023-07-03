bc6b032cfc6f6ef5dd943b79245ef1be85d258d4
Juangmcz <juangui-089@hotmail.com>
2023-07-03 15:28:07 -0500

    created subscriber model

---
+++ src/app/models/subscriber.model.ts
@@ -0,0 +1,26 @@
+export interface Subscriber {
+  SystemId: null;
+  Area: 'Tecnología y Ciencia';
+  PublicId: 775;
+  CountryCode: 'VN';
+  CountryName: 'Vietnam';
+  Name: 'Tony Stark';
+  EndpointsCount: 0;
+  Email: 'stark@stark.co';
+  JobTitle: 'CEO de Stark Industries';
+  PhoneNumber: '5551234567';
+  PhoneCode: '84';
+  PhoneCodeAndNumber: '(84) 5551234567';
+  LastActivityUtc: null;
+  LastActivity: null;
+  LastActivityString: null;
+  SubscriptionDate: null;
+  SubscriptionMethod: 0;
+  SubscriptionState: 0;
+  SubscriptionStateDescription: 'Pending';
+  Topics: [];
+  ValidEmail: true;
+  Activity: '--';
+  ConnectionState: 2;
+  Id: 9801;
+}
