7e624dd56f380baaef1c9d009b35e543030a80dd
Juangmcz <juangui-089@hotmail.com>
2023-07-03 10:55:29 -0500

    added login-page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login-page', pathMatch: 'full' },
+  {
+    path: 'login-page',
+    component: LoginPageComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
