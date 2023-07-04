b015f79201fc7bbbfb8ae397c3f3245b145fd0bc
Juangmcz <juangui-089@hotmail.com>
2023-07-04 10:28:58 -0500

    login page is working as expected

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { LoginResponse } from 'src/app/models/login-response.model';
 import { Login } from 'src/app/models/login.model';
 import { LoginService } from 'src/app/services/login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login-page',
@@ -16,7 +17,8 @@ export class LoginPageComponent {
   constructor(
     private builder: FormBuilder,
     private router: Router,
-    private service: LoginService
+    private loginService: LoginService,
+    private cookieService: CookieService
   ) {}
 
   ngOnInit(): void {
@@ -32,11 +34,11 @@ export class LoginPageComponent {
       Password: this.form.value.password,
     };
 
-    this.service.login(admin).subscribe((data) => {
+    this.loginService.login(admin).subscribe((data) => {
       let loginResponse: LoginResponse = data;
 
       if (loginResponse.Status === 1) {
-        localStorage.setItem('Token', loginResponse.Token);
+        this.cookieService.set('Token', loginResponse.Token);
         this.router.navigate(['manage-subscribers']);
       }
     });
