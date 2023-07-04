4169f2bdf23bafda01437ffde6c4f30022a689f3
Juangmcz <juangui-089@hotmail.com>
2023-07-04 10:29:26 -0500

    implemented subscriber service

--- src/app/services/subscriber.service.ts
+++ src/app/services/subscriber.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { SubscriberInfo } from '../models/subscriber-info.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubscriberService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  api: string = 'https://lab.app.invertebrado.co/api/subscribers';
+
+  getAllSubscribers(): Observable<any> {
+    return this.http.get(this.api);
+  }
+
+  getSubscriberById(id: number) {
+    return this.http.get(`${this.api}/${id}`);
+  }
+
+  createSubscriber(subscriber: SubscriberInfo) {
+    return this.http.post(this.api, subscriber);
+  }
+
+  updateSubscriber(subscriber: SubscriberInfo): Observable<any> {
+    return this.http.put(this.api, subscriber);
+  }
+
+  deleteSubscriber(id: number) {
+    return this.http.delete(`${this.api}/${id}`);
+  }
 }
