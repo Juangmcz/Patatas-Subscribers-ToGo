5310ce3fc3f7c31190e7dd88d631a724d5ab1c84
Juangmcz <juangui-089@hotmail.com>
2023-07-04 10:28:23 -0500

    implemented manage subscribers page

--- src/app/pages/manage-subscribers/manage-subscribers.component.ts
+++ src/app/pages/manage-subscribers/manage-subscribers.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SubscriberInfo } from 'src/app/models/subscriber-info.model';
+import { SubscriberService } from 'src/app/services/subscriber.service';
 
 @Component({
   selector: 'app-manage-subscribers',
   templateUrl: './manage-subscribers.component.html',
-  styleUrls: ['./manage-subscribers.component.scss']
+  styleUrls: ['./manage-subscribers.component.scss'],
 })
-export class ManageSubscribersComponent {
+export class ManageSubscribersComponent implements OnInit {
+  subscribers: SubscriberInfo[] = [];
+  total: number = this.subscribers.length;
 
+  constructor(private service: SubscriberService) {}
+
+  ngOnInit(): void {
+    this.service.getAllSubscribers().subscribe({
+      next: (subscriber) => {
+        console.log(subscriber.data);
+        (this.subscribers = subscriber.Data),
+          (this.total = this.subscribers.length);
+      },
+      error: console.log,
+      complete: console.log,
+    });
+  }
 }
