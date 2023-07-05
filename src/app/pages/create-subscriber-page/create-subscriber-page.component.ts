c717f71e18b60976a7bb81bcae93966a1f6a52c8
Juangmcz <juangui-089@hotmail.com>
2023-07-04 20:59:40 -0500

    create subscriber is working

--- src/app/pages/create-subscriber-page/create-subscriber-page.component.ts
+++ src/app/pages/create-subscriber-page/create-subscriber-page.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SubscriberService } from 'src/app/services/subscriber.service';
 
 @Component({
   selector: 'app-create-subscriber-page',
   templateUrl: './create-subscriber-page.component.html',
-  styleUrls: ['./create-subscriber-page.component.scss']
+  styleUrls: ['./create-subscriber-page.component.scss'],
 })
-export class CreateSubscriberPageComponent {
+export class CreateSubscriberPageComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
 
+  data: any = {
+    Subscribers: [],
+  };
+
+  constructor(
+    private router: Router,
+    private builder: FormBuilder,
+    private subscriberService: SubscriberService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.builder.group({
+      Name: ['', [Validators.required, Validators.minLength(3)]],
+      Email: ['', [Validators.required, Validators.email]],
+      CountryCode: ['', [Validators.required, Validators.minLength(2)]],
+      PhoneNumber: ['', [Validators.required, Validators.minLength(4)]],
+      JobTitle: '',
+      Area: '',
+      Topics: [[]],
+    });
+  }
+
+  onSubmit() {
+    this.data['Subscribers'].push(this.form.value);
+    this.subscriberService
+      .createSubscriber(this.data)
+      .subscribe((answer) => console.log(answer));
+    alert('This subscriber has been created');
+  }
+
+  onBack() {
+    if (confirm('Are you sure you want to exit this menu?')) {
+      this.router.navigate(['manage-subscribers']);
+    }
+  }
 }
