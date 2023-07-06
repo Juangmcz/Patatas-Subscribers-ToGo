d9ae67a12bd9ec01a5e4ba7d2644f606dcf0c0b3
Juangmcz <juangui-089@hotmail.com>
2023-07-05 22:08:48 -0500

    added refreshToken method

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { LoginResponse } from '../models/login-response.model';
 import { Login } from '../models/login.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +11,20 @@ import { Login } from '../models/login.model';
 export class LoginService {
   private api: string = 'https://lab.app.invertebrado.co/api/account/login';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   login(form: Login): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(this.api, form);
+  }
+
+  refreshToken(refreshToken: string) {
+    return this.http
+      .post<any>(`${this.api}account/regenerateToken`, { refreshToken })
+      .pipe(
+        tap((response) => {
+          this.cookieService.set('Token', response.Token);
+          this.cookieService.set('RefreshToken', response.RefreshToken);
+        })
+      );
   }
 }
