79ea7dbe9ec4e81b2fc788eaf9a11fa9f8bdfa50
Juangmcz <juangui-089@hotmail.com>
2024-02-17 13:02:13 -0500

    Refactor app routing and configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [SidenavbarComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'patatas-subscribers-togo';
---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { JwtInterceptor } from './commons/jwt.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
+};
